00783d0e2c092732c1430dbc1277348a0fe37293
Alex Comeau <17982529+alexm622@users.noreply.github.com>
2022-11-17 13:42:48 -0500

    fixed bad location names

--- api_integration/get_loc_data.py
+++ api_integration/get_loc_data.py
@@ -1,7 +1,8 @@
-#openweathermap python program
+# openweathermap python program
 from argparse import Namespace
 import requests
 import json
+import sys
 from datac.location import Location
 
 from general_utils import read_secrets
@@ -10,27 +11,37 @@ import database_integration.push_data as pd
 import api_integration.loc_to_weather as ltw
 
 locs_file: str = "places.csv"
-skip_geocode:bool = False
+skip_geocode: bool = False
 # returns tuple[lon,lat]
 def get_coords(location: str, secrets: dict[str, str]) -> tuple[float, float]:
-    #break into pieces, format is city_name,country_code,state_code
+    # break into pieces, format is city_name,country_code,state_code
     state_code = ""
-    city_name = location.split(',')[0]
-    country_code = location.split(',')[1]
-    if location.count(',') > 2:
-        state_code = location.split(',')[2]
+    city_name = location.split(",")[0]
+    country_code = location.split(",")[1]
+    if location.count(",") > 2:
+        state_code = location.split(",")[2]
 
-    request_geocode:str = "http://api.openweathermap.org/geo/1.0/direct?q=$%,$&limit=$&appid=@"
+    request_geocode: str = (
+        "http://api.openweathermap.org/geo/1.0/direct?q=$%,$&limit=$&appid=@"
+    )
     if state_code != "":
         request_geocode = request_geocode.replace("%", "," + state_code)
     else:
-        request_geocode = request_geocode.replace("%", "");
+        request_geocode = request_geocode.replace("%", "")
 
-    request_geocode = request_geocode.replace("$", city_name, 1).replace("$", country_code, 1).replace("$",str(1), 1).replace("@", str(secrets.get("owm")))
+    request_geocode = (
+        request_geocode.replace("$", city_name, 1)
+        .replace("$", country_code, 1)
+        .replace("$", str(1), 1)
+        .replace("@", str(secrets.get("owm")))
+    )
 
     response1 = requests.get(request_geocode)
-
-    geocoding: dict = json.loads(response1.text[1: -1])
+    try:
+        geocoding: dict = json.loads(response1.text[1:-1])
+    except:
+        print(city_name, "failed", sep=" ")
+        return 0.0, 0.0
     try:
         geocoding.pop("local_names")
     except:
@@ -40,45 +51,45 @@ def get_coords(location: str, secrets: dict[str, str]) -> tuple[float, float]:
     lon: str | None = str(geocoding.get("lon"))
     if lat == None or lon == None:
         print("city ", city_name, "failed get request")
-        return 0.0,0.0
+        return 0.0, 0.0
     else:
         return float(lon), float(lat)
 
 
-
-def locations_to_coords(locations: list[str], secrets: dict[str,str]):
-    mappings: dict[str, tuple[float,float]] = {}
+def locations_to_coords(locations: list[str], secrets: dict[str, str]):
+    mappings: dict[str, tuple[float, float]] = {}
     for loc in locations:
         coords = get_coords(loc, secrets)
         statecode = ""
         if loc.count(",") == 2:
             statecode = " " + loc.split(",")[2]
-        locname:str = loc.split(',')[0] + " " + loc.split(',')[1] + statecode
+        locname: str = loc.split(",")[0] + " " + loc.split(",")[1] + statecode
         mappings.update({locname: coords})
-        
+
     return mappings
+
 
 def load_locs():
     global locs_file
     locs: list[str] = []
     f = open(locs_file, "r")
-    f.readline() #drop first line
+    f.readline()  # drop first line
     for s in f:
         if not "," in s:
             continue
-        s = s.strip("\n").strip(",") 
+        s = s.strip("\n").strip(",")
         locs.append(s)
     return locs
 
 
-def write_geodata(data: dict[str, tuple[float,float]]) -> bool:
-    f = open("loc_data.csv","w")
+def write_geodata(data: dict[str, tuple[float, float]]) -> bool:
+    f = open("loc_data.csv", "w")
     f.write("location,lon,lat\n")
     for s in data.keys():
         csv_line = s
-        tup  = data.get(s)
-        lon:float
-        lat:float
+        tup = data.get(s)
+        lon: float
+        lat: float
         if tup == None:
             print("incorrect value in tuple for " + s)
             return False
@@ -88,15 +99,16 @@ def write_geodata(data: dict[str, tuple[float,float]]) -> bool:
         csv_line += "," + str(lon) + "," + str(lat)
         f.write(csv_line + "\n")
 
-
     return False
 
-def valid_file(file:str) -> bool:
+
+def valid_file(file: str) -> bool:
     print("validating file")
     return False
-    
+
+
 def push_locations():
-    f = open("loc_data.csv", 'r')
+    f = open("loc_data.csv", "r")
     f.readline()
     for l in f:
         l = l.strip()
@@ -121,12 +133,13 @@ def push_locations():
 
         pd.push_location(name, country_code, state_code, lon, lat)
 
-def locnames_to_data(args:Namespace|None=None) -> bool:
+
+def locnames_to_data(args: Namespace | None = None) -> bool:
     global skip_geocode
     if args != None and not (len(vars(args)) == 0):
-        assert(args is not None)
+        assert args is not None
         if not args.geocode:
-            print('skipping geocoding')
+            print("skipping geocoding")
             skip_geocode = True
         if args.push_loc:
             print("pushing locations")
@@ -139,7 +152,7 @@ def locnames_to_data(args:Namespace|None=None) -> bool:
                 locs_file = args.LOCS
             else:
                 print("invalid locs file")
-                print("using default (places.csv)")   
+                print("using default (places.csv)")
 
     locations: list[str] = load_locs()
 
@@ -149,15 +162,7 @@ def locnames_to_data(args:Namespace|None=None) -> bool:
         print("api key not properly defined")
         exit(1)
 
-    coords: dict[str, tuple[float,float]] = locations_to_coords(locations, secrets);
+    coords: dict[str, tuple[float, float]] = locations_to_coords(locations, secrets)
 
     write_geodata(coords)
     return True
-
-
-
-
-
-
-
-
